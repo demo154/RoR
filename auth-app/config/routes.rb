d834ea275fa44ddb40dc8076d9400359e616786d
grajeshlr <rajeshprlrld@gmail.com>
2020-02-14 16:14:23 +0530

    deleted

--- auth-app/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  resources :items
-  post 'authenticate', to: 'authentication#authenticate'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
